164ee5f57f70230374779f89241604d4ac4facf2
Alan Anaya <hello@alananaya.dev>
2023-08-10 19:52:57 -0600

    Initial Commit

---
+++ Simple Calculator/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Simple Calculator
+//
+//  Created by Alan Anaya Araujo on 10/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Simple Calculator/Simple_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_CalculatorApp.swift
+//  Simple Calculator
+//
+//  Created by Alan Anaya Araujo on 10/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Simple_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
